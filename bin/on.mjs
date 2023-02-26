f8fa27cd34e61f8937b53ca441c706829c4150c8
Martin Zheng <jszhengyq@gmail.com>
2023-02-26 09:15:57 -0800

    Added mole-yarn and mole-on

---
+++ bin/on.mjs
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+import copyModelSettings from "../lib/copy-model-settings.js";
+import updatePackageJSONSettings from "../lib/update-package-json-settings.js";
+
+copyModelSettings();
+updatePackageJSONSettings();
