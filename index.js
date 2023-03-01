4d68ba483d320344680c9c0b9ab0dc4a21b660da
Martin Zheng <jszhengyq@gmail.com>
2023-02-28 23:55:12 -0800

    Changed runnable to main index.js so it's more convenient to be called by npx

---
+++ index.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+import yargs from 'yargs/yargs';
+import runCommand from '../lib/run-command.js';
+import initializeSettings from '../lib/initialize-settings.js';
+import packageJSON from '../package.json' assert { type: 'json' };
+
+const args = process.argv.slice(2);
+
+const argv = yargs(args)
+  .alias('n', 'new')
+  .describe('n', 'If specified, it will initialize development settings, including TypeScript, Jest, ESLint, etc., install dependencies, and run the command')
+  .alias('c', 'command')
+  .string('c')
+  .demandOption('c')
+  .describe('c', 'Command to run')
+  .help('h')
+  .alias('h', 'help')
+  .alias('v', 'version')
+  .version(packageJSON.version)
+  .usage('Usage: mole [-y][-n] -c <command>')
+  .example('mole -c "yarn test"', 'Run "yarn test"')
+  .example('mole -c -n "yarn test"', 'Initialize the settings and run "yarn test"')
+  .argv;
+
+if (argv.n) {
+  initializeSettings();
+}
+
+runCommand(argv.c, argv.n);
