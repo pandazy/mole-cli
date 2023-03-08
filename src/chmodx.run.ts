48396be161fbf91f8beb780fae24867035de2ede
Martin Zheng <jszhengyq@gmail.com>
2023-03-07 22:06:08 -0800

    Corrected chmodx logic

--- src/chmodx.run.ts
+++ src/chmodx.run.ts
@@ -10,13 +10,17 @@ assertMoleCliOnly();
 
 const distFolder = getUserPath('dist');
 
+const reservedEntries = Object.freeze(new Set(['index.js']));
+
 let executableCount = 0;
 fs.readdirSync(distFolder).forEach((file) => {
-  if (file.endsWith('.run.js') || file !== 'chmodx.run.js') {
-    const executablePath = `${distFolder}/${file}`;
-    execSync(`chmod +x ${executablePath}`, { stdio: 'inherit' });
-    executableCount += 1;
+  if ((!reservedEntries.has(file) && !file.endsWith('.run.js')) || file === 'chmodx.run.js') {
+    return;
   }
+  const executablePath = `${distFolder}/${file}`;
+  execSync(`chmod +x ${executablePath}`, { stdio: 'inherit' });
+  console.log(chalk.green(`Executable file created: ${executablePath}`));
+  executableCount += 1;
 });
 
 if (executableCount <= 0) {
